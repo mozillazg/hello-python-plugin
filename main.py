a9569f441db9e25ebb1ec6fae57e97272667a971
Mozillazg <opensource.mozillazg@gmail.com>
2013-08-08 13:32:36 +0800

    添加命令行选项（--enable）控制启用哪些插件。

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+
 from utils import import_string, find_modules
 
 
+def parse_args():
+    """解析命令行参数."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--enable', action='append', dest='plugins',
+                        default=[], help='enable some plugins (default: [])')
+    return parser.parse_args()
+
+
 def main():
+    args = parse_args()
     text = 'test plugin.'
     print 'Default: %s' % text
 
     modules = find_modules('plugins')
     for module in modules:
-        plugin = import_string(module)
-        print plugin.output(text)
+        enabled_plugins = ['plugins.' + x for x in args.plugins]
+        if module in enabled_plugins:
+            plugin = import_string(module)
+            print plugin.output(text)
 
 if __name__ == '__main__':
     main()
